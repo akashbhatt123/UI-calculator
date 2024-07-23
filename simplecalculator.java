3e43a97b2f4de7851bf8c3df3061feab6829ace6
Akash Bhattt <114553046+akashbhatt123@users.noreply.github.com>
2024-07-23 07:08:54 +0530

    Update simplecalculator.java

--- simplecalculator.java
+++ simplecalculator.java
@@ -1,56 +1,174 @@
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-class Main {
-  public static void main(String[] args) {
-
+public class Calculator implements ActionListener
+{
+    JFrame frame;
+    JTextField textfield;
+    JButton[] numberButtons=new JButton[10];
+    JButton[] functionButtons=new JButton[9];
+    JButton addButton,subButton,mulButton,divButton;
+    JButton decButton, equButton,delButton,clrButton,negButton;
+    JPanel panel;
+    Font fontCalc=new Font("Calibri",Font.BOLD,30);
+    double num1=0,num2=0,result=0;
     char operator;
-    Double number1, number2, result;
+    Calculator()
+    {
+        frame=new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420,550);
+        frame.setLayout(null);
 
-    // create an object of Scanner class
-    Scanner input = new Scanner(System.in);
+        textfield=new JTextField();
+        textfield.setBounds(50,25,300,50);
+        textfield.setFont(fontCalc);
+        textfield.setEditable(false);
 
-    // ask users to enter operator
-    System.out.println("Choose an operator: +, -, *, or /");
-    operator = input.next().charAt(0);
+        addButton=new JButton("+");
+        subButton=new JButton("-");
+        mulButton=new JButton("*");
+        divButton=new JButton("/");
+        decButton=new JButton(".");
+        equButton=new JButton("=");
+        delButton=new JButton("Del");
+        clrButton=new JButton("Clear");
+        negButton=new JButton("(-)");
 
-    // ask users to enter numbers
-    System.out.println("Enter first number");
-    number1 = input.nextDouble();
+        functionButtons[0]=addButton;
+        functionButtons[1]=subButton;
+        functionButtons[2]=mulButton;
+        functionButtons[3]=divButton;
+        functionButtons[4]=decButton;
+        functionButtons[5]=equButton;
+        functionButtons[6]=delButton;
+        functionButtons[7]=clrButton;
+        functionButtons[8]=negButton;
 
-    System.out.println("Enter second number");
-    number2 = input.nextDouble();
+        for (int i=0;i<9;i++)
+        {
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(fontCalc);
+            functionButtons[i].setFocusable(false);
+        }
+        for (int i=0;i<10;i++)
+        {
+            numberButtons[i]=new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(fontCalc);
+            numberButtons[i].setFocusable(false);
+        }
 
-    switch (operator) {
+        negButton.setBounds(50,430,100,50);
+        delButton.setBounds(150,430,100,50);
+        clrButton.setBounds(250,430,100,50);
 
-      // performs addition between numbers
-      case '+':
-        result = number1 + number2;
-        System.out.println(number1 + " + " + number2 + " = " + result);
-        break;
+        panel=new JPanel();
+        panel.setBounds(50,100,300,300);
+        panel.setLayout(new GridLayout(4,4,10,10));
 
-      // performs subtraction between numbers
-      case '-':
-        result = number1 - number2;
-        System.out.println(number1 + " - " + number2 + " = " + result);
-        break;
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButton);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(mulButton);
+        panel.add(decButton);
+        panel.add(numberButtons[0]);
+        panel.add(equButton);
+        panel.add(divButton);
 
-      // performs multiplication between numbers
-      case '*':
-        result = number1 * number2;
-        System.out.println(number1 + " * " + number2 + " = " + result);
-        break;
-
-      // performs division between numbers
-      case '/':
-        result = number1 / number2;
-        System.out.println(number1 + " / " + number2 + " = " + result);
-        break;
-
-      default:
-        System.out.println("Invalid operator!");
-        break;
+        frame.add(panel);
+        frame.add(negButton);
+        frame.add(delButton);
+        frame.add(clrButton);
+        frame.add(textfield);
+        frame.setVisible(true);
     }
-
-    input.close();
-  }
+    public static void main(String[] args)
+    {
+        Calculator calc=new Calculator();
+    }
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        for (int i=0;i<10;i++)
+        {
+            if (e.getSource()==numberButtons[i])
+            {
+                textfield.setText(textfield.getText().concat(String.valueOf(i)));
+            }
+        }
+        if (e.getSource()==decButton)
+        {
+            textfield.setText(textfield.getText().concat(String.valueOf(".")));
+        }
+        if (e.getSource()==addButton)
+        {
+            num1=Double.parseDouble(textfield.getText());
+            operator='+';
+            textfield.setText("");
+        }
+        if (e.getSource()==subButton)
+        {
+            num1=Double.parseDouble(textfield.getText());
+            operator='-';
+            textfield.setText("");
+        }
+        if (e.getSource()==mulButton)
+        {
+            num1=Double.parseDouble(textfield.getText());
+            operator='*';
+            textfield.setText("");
+        }
+        if (e.getSource()==divButton)
+        {
+            num1=Double.parseDouble(textfield.getText());
+            operator='/';
+            textfield.setText("");
+        }
+        if (e.getSource()==equButton)
+        {
+            num2=Double.parseDouble(textfield.getText());
+            switch(operator)
+            {
+                case '+':result=num1+num2;
+                         break;
+                case '-':result=num1-num2;
+                         break;
+                case '*':result=num1*num2;
+                         break;
+                case '/':result=num1/num2;
+                         break;
+            }
+            textfield.setText(String.valueOf(result));
+            num1=result;
+        }
+        if (e.getSource()==clrButton)
+        {
+            textfield.setText("");
+        }
+        if (e.getSource()==delButton)
+        {
+            String str=textfield.getText();
+            textfield.setText("");
+            for (int i=0;i<str.length()-1;i++)
+            {
+                textfield.setText(textfield.getText()+str.charAt(i));
+            }
+        }
+        if (e.getSource()==negButton)
+        {
+            Double temp=Double.parseDouble(textfield.getText());
+            temp*=-1;
+            textfield.setText(String.valueOf(temp));
+        }
+    }
 }
